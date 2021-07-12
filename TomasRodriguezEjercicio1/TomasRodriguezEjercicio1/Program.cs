36cd0184b252de5c3cfcd9006a4e04e997a31377
Llirakor <87139047+Llirakor@users.noreply.github.com>
2021-07-12 20:16:21 -0300

    Add files via upload

---
+++ TomasRodriguezEjercicio1/TomasRodriguezEjercicio1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TomasRodriguezEjercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado1, lado2;
+
+            Console.Write("Ingrese El Lado 1: ");
+            lado1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese El Lado 2: ");
+            lado2 = int.Parse(Console.ReadLine());
+
+            double Hipotenusa = Math.Sqrt((lado1 * lado1) + (lado2 * lado2));
+            Console.WriteLine("La hipotenusa es: " + Hipotenusa);
+
+            double Superficie = (lado1 + lado2) / 2;
+            Console.WriteLine("El perimetro es: " + Superficie);
+
+            double Perimetro = lado1 + lado2 + Hipotenusa;
+            Console.WriteLine("El perimetro es: " + Perimetro);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TomasRodriguezEjercicio2/TomasRodriguezEjercicio2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TomasRodriguezEjercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que determina el tipo de triangulo segun sus lados ");
+            Console.WriteLine("Ingrese el lado 1: ");
+            int lado1;
+            lado1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el lado 2: ");
+            int lado2;
+            lado2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el lado 3: ");
+            int lado3;
+            lado3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Los lados son {0},{1} y {2}", lado1, lado2, lado3);
+
+            if (lado1 == lado2 ^ lado1 == lado2 ^ lado2 == lado3)
+            {
+                Console.WriteLine("El triángulo es equilatero");
+            }
+            else if ((lado1 != lado2) ^ (lado1 != lado3) ^ (lado2 != lado3))
+            {
+                Console.WriteLine("El triángulo es escaleno");
+            }
+            else
+            {
+                Console.WriteLine("El triángulo es isósceles");
+            }
+            Console.ReadLine();
+        }
+    }
+}
